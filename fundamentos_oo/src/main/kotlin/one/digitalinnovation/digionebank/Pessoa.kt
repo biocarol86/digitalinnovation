4725e5c2940b3d1753c95362eb94e7195522ff01
Maria Carolina <biocarol86@gmail.com>
2021-06-26 18:38:46 -0300

    created person class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,18 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa(
-    val nome: String,
-    val cpf: String
-)
+
+class Pessoa {
+    val nome: String = "Jether"
+    val cpf: String = "233.455.333-00"
+
+    inner class Endereco{
+        var rua: String = "Rua do Teste"
+    }
+}
+fun main (){
+    val jether = Pessoa()
+    
+    println(jether.nome)
+    println(jether.cpf)
+}
 
