7c3826141e93a7d2513345084b6a2b26ef7c4aa5
Maria Carolina <biocarol86@gmail.com>
2021-06-27 11:53:59 -0300

    Modificação do CPF para private set

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,17 @@ package one.digitalinnovation.digionebank
 
 
 class Pessoa {
-    val nome: String = "Jether"
-    val cpf: String = "233.455.333-00"
+    var nome: String = "Jether"
+
+    var cpf: String = "233.455.333-00"
+    private set //Só posso mexer no cpf no escopo da classe (set)-> função que atribui valor
 
 
 }
 fun main (){
     val jether = Pessoa()
     
+    println(jether)
     println(jether.nome)
     println(jether.cpf)
 }
